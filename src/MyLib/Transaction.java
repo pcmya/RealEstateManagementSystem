85c7e711725165ace6af5041b15ddacb7ab0a5dd
Emarie Azares <azares.emarie@gmail.com>
2026-03-10 20:15:01 +0800

    transac

--- src/MyLib/Transaction.java
+++ src/MyLib/Transaction.java
@@ -9,7 +9,18 @@ package MyLib;
  * @author pcmya
  */
 public class Transaction {
+    private double payment;
 
+    public Transaction() {
+    }
+    
+    public void computePayment(){
+        
+    }
+    
+    public void printReceipt(){
+        
+    }
     
     
 }
