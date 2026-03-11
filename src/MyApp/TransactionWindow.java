974d4ca5dc490225dceb032376c793c02da12a8a
Emarie Azares <azares.emarie@gmail.com>
2026-03-12 07:47:20 +0800

    tw

--- src/MyApp/TransactionWindow.java
+++ src/MyApp/TransactionWindow.java
@@ -191,7 +191,7 @@ public class TransactionWindow extends javax.swing.JFrame {
                                     .addGroup(receiptPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                                         .addComponent(jLabel32, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addComponent(jLabel33, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jLabel21, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)))
+                                        .addComponent(jLabel21, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 102, Short.MAX_VALUE)))
                                 .addGap(0, 0, Short.MAX_VALUE)))))
                 .addContainerGap())
         );
@@ -368,7 +368,8 @@ public class TransactionWindow extends javax.swing.JFrame {
     private void confirmTransactBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_confirmTransactBtnActionPerformed
         // TODO add your handling code here:
         String buyReserveSelect = comboBuyReserve.getSelectedItem().toString();
-        
+        //String payMethodSelect = comboPayMethod.getSelectedItem().toString();
+        //to be continued
         if (buyReserveSelect.equals("Reserve Lot")) {
             chosenBlkLot.setStatus("Reserved");
         }
@@ -389,11 +390,6 @@ public class TransactionWindow extends javax.swing.JFrame {
         dispose();
         
     }//GEN-LAST:event_confirmTransactBtnActionPerformed
-<<<<<<< HEAD
-=======
-        
-    }                                        
->>>>>>> 505d98f (tesT)
 
     private void comboBuyReserveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboBuyReserveActionPerformed
         // TODO add your handling code here:
